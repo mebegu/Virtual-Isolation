1b5a9f45adda47f52f262b03de322d2fcc9cb039
cangumeli <cgumeli@ku.edu.tr>
2017-04-29 12:13:11 +0300

    Socket last

--- socket.js
+++ socket.js
@@ -2,6 +2,14 @@ module.exports = server => {
    const io = require('socket.io')(server)
    io.on('connection', client => {
       console.log('Client connected')
+
+      client.on('enterRoom', data => {
+
+      })
+      client.on('message', data => {
+         console.log(data)
+      })
    })
    console.log('Socket created...')
+   return io
 }
